6f70d6e9367b550687e17f4a22ea68e0fd5a9c35
RobertBenyamin <robertbenyamino@gmail.com>
2024-09-19 13:17:43 +0700

    Style Navbar component to add responsive navigation menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   const isAuthenticated = !!localStorage.getItem("token");
+  const handleToggle = () => setIsOpen(!isOpen);
 
   return (
-    <Flex justifyContent="space-between" p="4" bg="teal.500">
+    <Flex
+      as="nav"
+      justifyContent="space-between"
+      p="4"
+      bg="teal.600"
+      color="white"
+      alignItems="center"
+    >
       <Box>
-        <Link to="/">Home</Link>
+        <Link to="/" style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+          Home
+        </Link>
       </Box>
-      <Box>
+
+      <IconButton
+        display={{ base: "block", md: "none" }}
+        onClick={handleToggle}
+        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+        variant="outline"
+        colorScheme="white"
+        aria-label="Toggle Navigation"
+      />
+
+      <HStack
+        as="nav"
+        spacing="8"
+        display={{ base: isOpen ? "flex" : "none", md: "flex" }}
+        flexDirection={{ base: "column", md: "row" }}
+        alignItems="center"
+        position={{ base: "absolute", md: "relative" }}
+        top={{ base: "60px", md: "0" }}
+        left={{ base: "0", md: "unset" }}
+        right={{ base: "0", md: "unset" }}
+        bg={{ base: "teal.600", md: "unset" }}
+        py={{ base: 4, md: 0 }}
+      >
         {isAuthenticated ? (
           <>
-            <Link to="/sell">Sell</Link>
-            <Link to="/buy">Buy</Link>
+            <Link to="/sell" className="nav-link">
+              Sell
+            </Link>
+            <Link to="/buy" className="nav-link">
+              Buy
+            </Link>
             <Button
               onClick={() => {
                 localStorage.removeItem("token");
                 window.location.href = "/";
               }}
+              bg="red.500"
+              textColor={"white"}
+              _hover={{ bg: "red.600" }}
             >
               Logout
             </Button>
           </>
         ) : (
           <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+            <Link to="/register" className="nav-link">
+              Register
+            </Link>
           </>
         )}
-      </Box>
+      </HStack>
     </Flex>
   );
 }
